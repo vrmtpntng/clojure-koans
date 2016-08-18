de6d1c3489182fe7bbc8050da7bc8cc3735aad4a
Swain R. Brooks <swain.brooks@gmail.com>
2016-08-18 11:49:42 -0600

    Commit

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -1,5 +1,6 @@
 (ns koans.10-runtime-polymorphism
-  (:require [koan-engine.core :refer :all]))
+  (:require [koan-engine.core :refer :all]
+            [clojure.string :as string]))
 
 (defn hello
   ([] "Hello World!")
@@ -10,19 +11,19 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :herbivore [a] (str (a :name) " eats veggies."))
+(defmethod diet :carnivore [a] (str (a :name) " eats animals."))
+(defmethod diet :default [a] (str "I don't know what " (a :name) " eats."))
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+  (= "Hello World!" (hello))
 
   "With one argument"
-  (= __ (hello "world"))
+  (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= __
+  (= "Hello to this group: Peter, Paul, Mary!"
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
